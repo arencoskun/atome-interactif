784a7a344669e7b594c58485240a8be0b9c663f8
arencoskun <ar3n1596@gmail.com>
2024-02-29 23:50:51 +0300

    Initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,5 +16,15 @@ const config: Config = {
     },
   },
   plugins: [],
+
+  screens: {
+    "2xs": { min: "300px" },
+    xs: { max: "575px" }, // Mobile (iPhone 3 - iPhone XS Max).
+    sm: { min: "576px", max: "897px" }, // Mobile (matches max: iPhone 11 Pro Max landscape @ 896px).
+    md: { min: "898px", max: "1199px" }, // Tablet (matches max: iPad Pro @ 1112px).
+    lg: { min: "1200px" }, // Desktop smallest.
+    xl: { min: "1259px" }, // Desktop wide.
+    "2xl": { min: "1359px" }, // Desktop widescreen.
+  },
 };
 export default config;
